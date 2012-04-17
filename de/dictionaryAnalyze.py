9870d2f6608b79ce195bd1af6449746b57fab952
weisseruebe <a.rettig@gmx.de>
2012-04-18 00:43:45 +0200

    Added CodeTable update

--- de/dictionaryAnalyze.py
+++ de/dictionaryAnalyze.py
@@ -4,9 +4,12 @@ Created on 10.04.2012
 @author: andreasrettig
 '''
 
-decWord = "Hbllpppi"
-dicWord = "Halloomo"
+decWord = "hillp"
+dicWord = "hallo"
 
+def find_key(dic, val):
+    """return the key of dictionary dic given the value"""
+    return [k for k, v in dic.iteritems() if v == val][0]
 
 def findWrongCharacters(decString, origString):
     charList = dict()
@@ -26,19 +29,33 @@ def findWrongCharacters(decString, origString):
         index += 1
     return charList
     
-def reduceDecryptTable(table, updateTable):
+def reduceDecryptTable(updateTable):
     newTable = dict()
     for char in updateTable:
         charDict = updateTable[char]
         newTable[char] = max(charDict, key=charDict.get)
     return newTable
 
+#Change the wrong output chars
+def updateCodeTable(codeTable, updateTable):
+    for char in updateTable:
+        print char
+        if char in codeTable.values():
+            key = find_key(codeTable,char)
+            codeTable[key] = updateTable[char]
+            print char+" -> "+codeTable[key]
+    return codeTable
+
 if __name__ == '__main__':
     newTable = findWrongCharacters(decWord,dicWord)
     codeTable = dict()
-    codeTable['a'] = 'z'
-    codeTable['z'] = 'a'
-    print newTable
-    print reduceDecryptTable(codeTable, newTable)
+    codeTable['h'] = 'h'
+    codeTable['k'] = 'i'
+    codeTable['l'] = 'l'
+    codeTable['m'] = 'p'
+
+    print codeTable
+    reduced = reduceDecryptTable(newTable)
+    print updateCodeTable(codeTable, reduced) 
     #print newTable
     
